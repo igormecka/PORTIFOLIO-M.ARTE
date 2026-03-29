307a41a360d6dd84b68ffbb25861daa2f9c11b37
igormecka <igormcm45@gmail.com>
2026-03-29 00:29:26 -0300

    Force WebP compression (-rw) and max height in Google Drive image proxy URLs to fix slow load time

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,15 +87,9 @@ function populateHero(images) {
   track.className = 'mural-track';
   container.appendChild(track);
   
-  // Duplicar imagens pra preencher a tela caso tenham poucas
-  let allImages = [];
-  while(allImages.length < 15) {
-     allImages = [...allImages, ...images];
-  }
-  
-  allImages.forEach((imgUrl) => {
+  images.forEach((imgUrl) => {
     const img = document.createElement('img');
-    img.src = imgUrl + '=s1920';
+    img.src = imgUrl + '=h800-rw';
     img.className = 'mural-img';
     track.appendChild(img);
   });
@@ -127,7 +121,7 @@ function populateAlbums(albums) {
     let isScrolling = false;
 
     div.innerHTML = `
-      <div class="album-thumb" style="background-image: url('${album.cover}=w800-h1000-p')">
+      <div class="album-thumb" style="background-image: url('${album.cover}=w800-h1000-p-rw')">
         <div class="album-thumb-icon" style="position:absolute;font-family:var(--serif);font-size:3rem;font-style:italic;color:rgba(201,169,110,0.3);top:50%;left:50%;transform:translate(-50%,-50%);">${initials}</div>
       </div>
       <div class="album-arrow" style="position:absolute;top:1rem;right:1rem;color:var(--gold-dim);opacity:0.8;">↗</div>
@@ -149,7 +143,7 @@ function populateAlbums(albums) {
             } else {
                 imgIdx = (imgIdx - 1 + album.images.length) % album.images.length;
             }
-            thumbBg.style.backgroundImage = `url('${album.images[imgIdx]}=w800-h1000-p')`;
+            thumbBg.style.backgroundImage = `url('${album.images[imgIdx]}=w800-h1000-p-rw')`;
             setTimeout(() => { isScrolling = false; }, 300); 
             e.stopPropagation(); 
         }
@@ -157,7 +151,7 @@ function populateAlbums(albums) {
     
     div.addEventListener('mouseleave', () => {
         imgIdx = 0;
-        thumbBg.style.backgroundImage = `url('${album.cover}=w800-h1000-p')`;
+        thumbBg.style.backgroundImage = `url('${album.cover}=w800-h1000-p-rw')`;
     });
 
     // Manter o clique pra abrir a janela original
@@ -192,7 +186,7 @@ function openWeddingDynamic(album) {
     div.onclick = () => openLightbox(imgUrl, album.images, i);
     
     const img = document.createElement('img');
-    img.src = imgUrl + '=w600-h800-p'; 
+    img.src = imgUrl + '=w600-h800-p-rw'; 
     img.loading = 'lazy'; // Lazy load for performance
     
     div.appendChild(img);
@@ -277,7 +271,7 @@ function openLightbox(src, allImages = [], index = 0) {
     thumbsContainer.innerHTML = '';
     currentLightboxImages.forEach((imgUrl, i) => {
       const thumb = document.createElement('img');
-      thumb.src = imgUrl + '=w150-h100-p';
+      thumb.src = imgUrl + '=w150-h100-p-rw';
       thumb.className = i === currentLightboxIndex ? 'lb-thumb active' : 'lb-thumb';
       thumb.onclick = (e) => {
         e.stopPropagation();
@@ -292,7 +286,7 @@ function openLightbox(src, allImages = [], index = 0) {
 function updateLightboxView() {
   const imgEl = document.getElementById('lightboxImg');
   if(!imgEl) return;
-  const src = currentLightboxImages[currentLightboxIndex] + '=s1920';
+  const src = currentLightboxImages[currentLightboxIndex] + '=s1600-rw';
   
   imgEl.style.opacity = 0;
   setTimeout(() => { 
